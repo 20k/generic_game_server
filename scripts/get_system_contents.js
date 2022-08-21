743293cb80cf2e4fe4fffd8b1eceb51586b8e31d
20k <icedshot@gmail.com>
2022-08-21 10:31:03 +0100

    really need to adjust whitespace

--- scripts/get_system_contents.js
+++ scripts/get_system_contents.js
@@ -12,10 +12,10 @@ function get_asteroid_description(asteroid)
 {
 	var largest_ore_amount = 0;
 	var largest_ore_index = 0;
-	
+
 	if(asteroid.ores.length == 0)
 		return "Barren";
-	
+
 	for(var i=0; i < asteroid.ores.length; i++)
 	{
 		if(asteroid.ores[i].ore_amount >= largest_ore_amount)
@@ -24,9 +24,9 @@ function get_asteroid_description(asteroid)
 			largest_ore_index = i;
 		}
 	}
-	
+
 	var largest_ore = asteroid.ores[largest_ore_index];
-	
+
 	return largest_ore.ore_name + " (" + largest_ore.ore_amount + ")";
 }
 
@@ -39,7 +39,7 @@ function format_poi_contents(poi)
 	for(var e of poi.contents)
 	{
 		types.push(e.name);
-		
+
 		if(e.type == "ship")
 		{
 			names.push("(\"" + e.nickname + "\")");
@@ -57,7 +57,7 @@ function format_poi_contents(poi)
 		{
 			names.push(e.nickname);
 		}
-		
+
 		positions.push("[" + e.position[0] + ", " + e.position[1] + "]");
 	}
 
@@ -83,7 +83,7 @@ function interactive_poi_contents(poi)
 	for(var e of poi.contents)
 	{
 		types.push(e.name);
-		
+
 		if(e.type == "ship")
 		{
 			names.push("(\"" + e.nickname + "\")");
@@ -105,7 +105,7 @@ function interactive_poi_contents(poi)
 		{
 			names.push("No Name");
 		}
-		
+
 		positions.push("[" + e.position[0] + ", " + e.position[1] + "]");
 	}
 
@@ -120,27 +120,27 @@ function interactive_poi_contents(poi)
 	/*var merged = array_concat(array_concat(fmt_1, fmt_2, ' | '), fmt_3, ' | ');
 
 	imgui.text(merged.join("\n"))*/
-	
+
 	for(var i=0; i < poi.contents.length; i++)
 	{
 		var e = poi.contents[i];
-		
+
 		var formatted_type = fmt_1[i];
 		var formatted_name = fmt_2[i];
 		var formatted_position = fmt_3[i];
-		
+
 		imgui.text(formatted_type + " | " + formatted_name + " | " + formatted_position);
-										
-		if(e.type == "ship" && e.owner == player.uid && player.controlling != e.uid) 
+
+		if(e.type == "ship" && e.owner == player.uid && player.controlling != e.uid)
 		{
 			/*imgui.sameline();
-			
+
 			imgui.text("hi" + e.uid);*/
-			
+
 			imgui.sameline();
 
-			if(imgui.smallbutton("Control ")) 
-			{			
+			if(imgui.smallbutton("Control "))
+			{
 				player.take_control(e);
 			}
 		}
@@ -156,42 +156,42 @@ function format_poi_name(poi)
 function format(arr)
 {
 	var max_length = 0;
-	
+
 	for(var i=0; i < arr.length; i++)
 	{
 		max_length = Math.max(arr[i].length, max_length);
 	}
-	
+
 	var result = [];
 	result.length = arr.length;
-	
+
 	for(var i=0; i < arr.length; i++)
 	{
 		var str = arr[i];
-		
+
 		str = str.padEnd(max_length, ' ');
-		
+
 		result[i] = str;
 	}
-	
+
 	return result
 }
 
 function array_concat(a1, a2, sep)
 {
 	var r = [];
-	
+
 	for(var i=0; i < a1.length; i++)
 	{
 		r.push(a1[i] + sep + a2[i]);
 	}
-	
+
 	return r;
 }
 
 function format_sys_contents(sys)
 {
-	//format_position(sys.position) 	
+	//format_position(sys.position)
 	var res = "System: " + sys.system_name + " " + format_position(sys.position) + "\n";
 
 	for(var poi of sys.contents)
@@ -210,63 +210,63 @@ function interactive_sys_contents(sys, player_view)
 {
 	///hacky
 	//var render_id = sys.uid * 129;
-	
-	imgui.pushstylecolor(21, 0, 0, 0, 0); 
-	imgui.pushstylecolor(22, 0, 0, 0, 0); 
-	imgui.pushstylecolor(23, 0, 0, 0, 0); 
-	
+
+	imgui.pushstylecolor(21, 0, 0, 0, 0);
+	imgui.pushstylecolor(22, 0, 0, 0, 0);
+	imgui.pushstylecolor(23, 0, 0, 0, 0);
+
 	var is_sys_open = player_view.is_sys_open(sys);
-	
+
 	var sys_str = "+";
-	
+
 	if(is_sys_open)
 		sys_str = "-";
-	
+
 	sys_str += "###" + render_id++;
-	
+
 	if(imgui.button(sys_str))
 	{
 		is_sys_open = !is_sys_open;
 		player_view.set_is_sys_open(sys, is_sys_open);
 	}
-	
+
 	imgui.sameline();
-	
+
 	imgui.text("System: " + sys.system_name + " " + format_position(sys.position) + "\n");
-	
-	if(!is_sys_open)	
+
+	if(!is_sys_open)
 	{
 		imgui.popstylecolor(3);
 		return;
 	}
-	
+
 	imgui.indent();
-	
+
 	for(var poi of sys.contents)
 	{
 		var title = format_poi_name(poi);
 
 		var is_open = player_view.is_poi_open(sys, poi);
-		
+
 		var str = "+";
-		
+
 		if(is_open)
 			str = "-";
-		
-		str += "###" + render_id++;			
+
+		str += "###" + render_id++;
 
 		if(imgui.button(str))
 		{
 			is_open = !is_open;
 			player_view.set_is_poi_open(sys, poi, is_open);
 		}
-		
+
 		imgui.sameline();
 
 		imgui.text(title);
-		
+
 		if(is_open)
-		{			
+		{
 			imgui.indent();
 			imgui.indent();
 			interactive_poi_contents(poi);
@@ -274,9 +274,9 @@ function interactive_sys_contents(sys, player_view)
 			imgui.unindent();
 		}
 	}
-	
+
 	imgui.unindent();
-	
+
 	imgui.popstylecolor(3);
 }
 
