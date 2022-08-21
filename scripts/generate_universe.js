6c1b7753b83b3017512b89a1cd5379dad9c32967
20k <icedshot@gmail.com>
2022-08-21 11:43:57 +0100

    beginning of mining, also commit generate_universe!

---
+++ scripts/generate_universe.js
@@ -0,0 +1,37 @@
+exec("object")
+exec("system")
+exec("poi")
+exec("universe")
+
+function generate_universe()
+{
+	var poi = make_poi("Asteroid Belt", "asteroidbelt", [20, 30]);
+
+	var owned_ship = make_ship([50, 40], "Stinky Names");
+
+	///aint great
+	//player.take_ownership(owned_ship);
+
+	player.take_ownership(poi.take(owned_ship));
+	
+	poi.take(make_ship([100, 20], "Also A Ship"));
+	poi.take(make_asteroid([150, 10]));
+	poi.take(make_asteroid([300, 10]));
+	poi.take(make_station([5, 223], "Owo station"));
+	poi.take(make_station([10, 9], "Stationary"));
+
+	var sys1 = make_system("Alpha Blenturi", [10, 10]);
+	var sys2 = make_system("Barnard's Spire", [15, 13]);
+
+	connect_systems(sys1, sys2);
+
+	sys1.take_poi(poi);
+
+	var universe = make_universe();
+	universe.take(sys1);
+	universe.take(sys2);
+	
+	return universe;
+}
+
+undefined
--- scripts/object.js
+++ scripts/object.js
@@ -9,13 +9,12 @@ function make_object_with_position(position)
 }
 
 function make_item(name, subtype)
-{
-	var obj = {};
-	obj.name = name;
-	obj.type = "item";
-	obj.subtype = subtype;
-	
-	return obj;
+{	
+	return {
+		name:name,
+		type:"item",
+		subtype:subtype	
+	};
 }
 
 function fill_asteroid(asteroid, ore_name, ore_type, ore_amount)
@@ -24,6 +23,19 @@ function fill_asteroid(asteroid, ore_name, ore_type, ore_amount)
 	item.ore_name = ore_name;
 	item.ore_type = ore_type;
 	item.ore_amount = ore_amount;
+	
+	item.take_ore_amount = function(amount) {		
+		if(amount > this.ore_amount) {
+			amount = this.ore_amount;
+		}
+		
+		var result = make_item("Ore", "ore");
+		result.ore_name = this.ore_name;
+		result.ore_type = this.ore_type;
+		result.ore_amount = this.ore_amount - amount;
+		
+		return result;
+	}
 	
 	asteroid.ores.push(item);
 }
@@ -39,6 +51,28 @@ function make_asteroid(position)
 	make_entity_actionable(obj);
 	
 	fill_asteroid(obj, "Titanium", "titanium", 15);
+	
+	///1 power removes 1 ore
+	obj.mine = function(total_power) {
+		var total_ore = 0;
+		
+		for(var e of this.ores) {
+			total_ore += e.ore_amount;
+		}
+		
+		if(total_ore < 0.0001)
+			return [];
+		
+		var result = [];
+		
+		var depleted_frac = total_power / total_ore;
+		
+		for(var item of this.ores) {
+			result.push(item.take_ore_amount(depleted_frac * item.ore_amount));
+		}
+		
+		return result;
+	}
 	
 	return obj;
 }
@@ -81,6 +115,11 @@ function make_ship(position, ship_name)
 	
 	obj.get_speed = function() {
 		return 1.;
+	}
+	
+	///per second
+	obj.get_mining_power = function() {
+		return 1;
 	}
 	
 	make_entity_actionable(obj);
