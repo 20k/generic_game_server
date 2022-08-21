65a9dec24b452989ac4ca04302bce147a1e13ea3
20k <icedshot@gmail.com>
2022-08-21 06:51:08 +0100

    action executor

--- scripts/action.js
+++ scripts/action.js
@@ -3,9 +3,7 @@ function make_move(e, finish_position, elased_time_s)
 	return {
 		object_uid: e.uid,
 		start: e.position,
-		finish: finish_position,
-		current_elapsed: 0,
-		finish_elapsed: elapsed_time_s
+		finish: finish_position
 	};
 }
 
@@ -16,10 +14,21 @@ function make_action()
 		subtype: "",
 		subobject: {},
 		
-		move_entity(e, finish_position, elased_time_s) {
+		current_elapsed: 0,
+		finish_elapsed: elapsed_time_s,
+		
+		remaining_time() {
+			return this.elapsed_time_s - this.current_elapsed;
+		},
+		
+		finished() {
+			return this.current_elapsed >= this.elapsed_time_s - 0.000001;
+		}
+		
+		/*move_entity(e, finish_position, elased_time_s) {
 			this.subtype = "move";
 			subobject = make_move(e, finish_position, elased_time_s);
-		}
+		}*/
 	};
 	
 	return obj;
@@ -33,7 +42,23 @@ function make_entity_actionable(obj)
 		this.actions.push(a);
 	}
 	
-	obj.add_action_time = function(delta_time_s) {
+	obj.add_action_time = function(delta_time_s, action_executor) {
+		var remaining = delta_time_s;
 		
+		while(actions.length > 0 && remaining > 0)
+		{		
+			var consumable = Math.min(actions[0].remaining_time(), remaining);
+			
+			action_executor(actions[0], consumable);
+			
+			actions[0].current_elapsed += consumable;
+			
+			remaining -= consumable;
+			
+			if(actions[0].finished())
+			{
+				actions.shift();
+			}
+		}
 	}
 }
